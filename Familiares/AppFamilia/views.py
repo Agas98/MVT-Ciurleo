2fb3163c00d6bedc143dabf823a96db355d70e6c
Augusto Ciurleo <ciurleoa98@gmail.com>
2022-06-11 18:49:10 -0300

    plantilla

--- Familiares/AppFamilia/views.py
+++ Familiares/AppFamilia/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from AppFamilia.models import parientesPorMadre, parientesPorPadre
 
-# Create your views here.
+def plantilla1(self):
+    madre = parientesPorMadre.objects.all()
+    padre = parientesPorPadre.objects.all()
+    diccionario = {'madre': madre, "padre": padre}
+    plantilla = loader.get_template('plantilla1.html')
+
+    documento = plantilla.render(diccionario)
+
+    return HttpResponse(documento)
